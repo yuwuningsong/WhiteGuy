66970cca0010ab3b39d1c09b7e762092ea660743
yuwuningsong <68041603+yuwuningsong@users.noreply.github.com>
2021-06-04 14:48:14 +0800

    开幕场景的人物2.5D移动
    WASD移动+转向，添加对应的Animation

---
+++ Assets/Scripts/PlayerWalkController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerWalkController : MonoBehaviour
+{
+    public float HorizontalInput { get; set; }
+    public float VerticalInput { get; set; }
+
+    [SerializeField] int walkSpeed = 0;
+    [SerializeField] Sprite up = null;
+    [SerializeField] Sprite down = null;
+    [SerializeField] Sprite left = null;
+    [SerializeField] Sprite right = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // TO DO: 碰撞闪烁
+        // TO DO: 地图素材层级
+        WalkX();
+        WalkY();
+    }
+
+    // WASD移动
+    void WalkX()
+    {
+        HorizontalInput = Input.GetAxisRaw("Horizontal");
+        if (HorizontalInput < 0)
+        {
+            GetComponent<SpriteRenderer>().sprite = left;
+            transform.position -= new Vector3(walkSpeed * Time.deltaTime, 0, 0); // 左转
+        }
+        else if (HorizontalInput > 0)
+        {
+            GetComponent<SpriteRenderer>().sprite = right;
+            transform.position += new Vector3(walkSpeed * Time.deltaTime, 0, 0); // 右转
+        }
+    }
+    void WalkY()
+    {
+        VerticalInput = Input.GetAxisRaw("Vertical");
+        if (VerticalInput < 0)
+        {
+            GetComponent<SpriteRenderer>().sprite = down;
+            transform.position -= new Vector3(0, walkSpeed * Time.deltaTime, 0); // 向下走
+        }
+        else if (VerticalInput > 0)
+        {
+            GetComponent<SpriteRenderer>().sprite = up;
+            transform.position += new Vector3(0, walkSpeed * Time.deltaTime, 0); // 向上走
+        }
+    }
+}
---
+++ Assets/Scripts/WalkAnimationController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WalkAnimationController : MonoBehaviour
+{
+    private Animator anim;
+    private PlayerWalkController walk;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        walk = GetComponent<PlayerWalkController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SetValues();
+    }
+
+    void SetValues()
+    {
+        anim.SetFloat("velocityX", walk.HorizontalInput);
+        anim.SetFloat("velocityY", walk.VerticalInput);
+    }
+}

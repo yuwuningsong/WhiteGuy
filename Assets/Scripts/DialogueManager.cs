08f9a9bb16a3aec80e719135642cf3e847fe062b
yuwuningsong <68041603+yuwuningsong@users.noreply.github.com>
2021-06-10 17:40:28 +0800

    完成开幕剧情对话，添加开幕动画

--- Assets/Scripts/DialogueManager.cs
+++ Assets/Scripts/DialogueManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DialogueManager : MonoBehaviour
 {
@@ -12,23 +13,39 @@ public class DialogueManager : MonoBehaviour
 
     [Header("DialogueUI")]
     [SerializeField] GameObject textBox = null;
+    [SerializeField] Text text = null;
 
-    private void Awake()
-    {
-        
-    }
+    [Header("TextFile")]
+    [SerializeField] TextAsset textFile = null;
+
+    private int index = 0;
+    private List<string> textLines = new List<string>();
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        GetTextFromFile(textFile);
+        index = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
         OpenTextBox();
-        CloseTextBox();
+        ToNextWord();
+        SkipDialogue();
+    }
+
+    // 导入文本文件
+    void GetTextFromFile(TextAsset textFile)
+    {
+        textLines.Clear(); // 清空
+        var lines = textFile.text.Split('\n');
+        foreach(var line in lines)
+        {
+            Debug.Log($"Line:{line.Replace("：", "：\n")}");
+            textLines.Add(line.Replace("：", "：\n"));
+        }
     }
 
     // 判断是否面对NPC并且在Talk范围内
@@ -49,7 +66,7 @@ public class DialogueManager : MonoBehaviour
     // 按下Z键弹出对话框
     void OpenTextBox()
     {
-        if (FaceNPC() && Input.GetKeyDown(KeyCode.Z))
+        if (!isTalking && FaceNPC() && Input.GetKeyDown(KeyCode.Z))
         {
             textBox.SetActive(true);
             isTalking = true;
@@ -58,13 +75,38 @@ public class DialogueManager : MonoBehaviour
     }
 
     // 按下Esc关闭对话框
-    void CloseTextBox()
+    void SkipDialogue()
     {
         if (isTalking && Input.GetKeyDown(KeyCode.Escape))
         {
-            textBox.SetActive(false);
-            isTalking = false;
-            player.canMove = true;
+            CloseTextBox();
         }
+    }
+
+    // 按下Z跳到下一句对话
+    void ToNextWord()
+    {
+        if (Input.GetKeyDown(KeyCode.Z) && IsOver()) CloseTextBox();
+        if (isTalking && Input.GetKeyDown(KeyCode.Z))
+        {
+            text.text = textLines[index];
+            index++;
+        }
+    }
+
+    // 判断对话是否结束
+    bool IsOver()
+    {
+        if (isTalking && index >= textLines.Count) return true;
+        return false;
+    }
+
+    // 关闭对话框
+    void CloseTextBox()
+    {
+        textBox.SetActive(false);
+        isTalking = false;
+        player.canMove = true;
+        index = 0;
     }
 }

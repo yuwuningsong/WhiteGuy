caccba0f10e5f688bd0e7d2b965177892a9dbc33
Yang <ddyyyj@qq.com>
2021-06-10 21:22:19 +0800

    竞技场人物移动跳跃
    基本的移动跳跃（未优化）

---
+++ Assets/Scripts/ArenaPlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArenaPlayerController : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float speed;
+    public float jumpForce;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Movement();
+    }
+
+    void Movement()
+    {
+        float horizontalMOVE = Input.GetAxis("Horizontal");
+        float faceDirection = Input.GetAxisRaw("Horizontal");
+
+        //左右移动转向
+        rb.velocity = new Vector2(horizontalMOVE * speed * Time.deltaTime, rb.velocity.y);
+        if (faceDirection != 0)
+        {
+            transform.localScale = new Vector3(faceDirection, 1, 1);
+        }
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce * Time.deltaTime);
+        }
+    }
+}

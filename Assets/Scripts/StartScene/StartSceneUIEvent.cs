4c7b7a7f5544aa1dd5fa40a7bba1f99262cde195
yuwuningsong <68041603+yuwuningsong@users.noreply.github.com>
2021-06-11 09:30:53 +0800

    完成开幕黑幕到开始剧情的过渡

---
+++ Assets/Scripts/StartScene/StartSceneUIEvent.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartSceneUIEvent : MonoBehaviour
+{
+    private bool startAnim = false;
+    private Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        anim.SetBool("startAnim", startAnim);
+        GetInput();
+    }
+
+    // 获取输入
+    void GetInput()
+    {
+        if (Input.GetKeyDown(KeyCode.Z)) // 按下Z播放动画
+        {
+            startAnim = true;
+        }
+    }
+
+    void CloseStartCurtain()
+    {
+        gameObject.SetActive(false);
+    }
+}

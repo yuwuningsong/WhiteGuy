c71d9ca95fa40377f7182bebc08f7e7038776595
yuwuningsong <68041603+yuwuningsong@users.noreply.github.com>
2021-06-25 16:50:21 +0800

    添加主角带剑攻击动画及剑物体

---
+++ Assets/Scripts/Player/PlayerFightController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFightController : MonoBehaviour
+{
+    [SerializeField] GameObject sword = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetInput();
+    }
+
+    // 获得输入
+    void GetInput()
+    {
+        if (Input.GetKeyDown(KeyCode.F)) // 按下F攻击
+        {
+            Fight();
+        }
+    }
+
+    // 攻击
+    void Fight()
+    {
+        sword.SetActive(true);
+    }
+}
---
+++ Assets/Scripts/Pokemon/SwordController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // 收回剑
+    void TakeBack()
+    {
+        gameObject.SetActive(false);
+    }
+}

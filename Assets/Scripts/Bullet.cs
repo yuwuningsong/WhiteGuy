8e041bc47457121b69bb33b1146396accfb1d821
yuwuningsong <68041603+yuwuningsong@users.noreply.github.com>
2021-06-27 12:56:19 +0800

    怪物攻击技能——落石

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    [SerializeField] int attack = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Player"))
+        {
+            collision.collider.GetComponent<PlayerHurtController>().health -= attack;
+            gameObject.SetActive(false);
+        }
+        if (collision.collider.CompareTag("AirWall"))
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/Monster/MonsterSkillController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterSkillController : MonoBehaviour
+{
+    [SerializeField] Transform skillOne = null;
+    [SerializeField] GameObject stone = null;
+    [SerializeField] float fallingCD = 0f;
+    [SerializeField] int stoneNum = 0;
+
+    private List<Action> skills = new List<Action>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        skills.Add(SkillOne);
+        StartSkill();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // 使用技能
+    void StartSkill()
+    {
+        skills[0].Invoke();
+    }
+
+    // 技能1
+    void SkillOne()
+    {
+        StartCoroutine("FallingStones");
+    }
+
+    // 技能1 落石生成子弹
+    IEnumerator FallingStones()
+    {
+        int index = 0;
+        while (index < stoneNum)
+        {
+            Vector3 position = new Vector3(UnityEngine.Random.Range(-10, 7), 7.3f, 0);
+            Instantiate(stone, position, new Quaternion(), skillOne);
+            index++;
+            yield return new WaitForSeconds(fallingCD);
+        }
+    }
+}

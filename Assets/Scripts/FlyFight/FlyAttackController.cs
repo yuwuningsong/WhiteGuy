ab798414617e720f2f25189c8bcbfa6ef93e30de
MYy-Program <1758336685@qq.com>
2021-06-28 19:09:36 +0800

    提交飞机大战背景、移动及发射子弹

---
+++ Assets/Scripts/FlyFight/FlyAttackController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyAttackController : MonoBehaviour
+{
+
+    //预置体
+    public GameObject bullet;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(bullet, new Vector2(transform.position.x+(float)0.8,transform .position .y), Quaternion.Euler(0, 0, -90));
+        }
+
+    }
+}

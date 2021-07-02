8346d9c82e82db202dc9a18aa47a1b0737f7c4f3
Chau56 <56555237+Chau56@users.noreply.github.com>
2021-07-02 22:51:43 +0800

    Parkour代码优化

--- Assets/Scripts/Player/Parkour/ParkourPlayerController.cs
+++ Assets/Scripts/Player/Parkour/ParkourPlayerController.cs
@@ -7,98 +7,44 @@ using UnityEngine.SceneManagement;
 public class ParkourPlayerController : MonoBehaviour
 {
     public Rigidbody2D rb;      //设置成私有，简化界面，调试时可在最前面加[SerializeField]
-    public Animator anim;
+    public Animator anim;       //动画
     public Collider2D coll;     //获得人物到地面的碰撞信息
-
-    //public float speed;         //速度
-    public float upspeed = 8;
-    public float jumpforce;
-    public LayerMask ground;    //下落碰地
-    public static int Hp = 1;   //血量
-    public int Count;
-
-    public Text CoinNum;
-
-    public bool IsJump = false;
-    public bool IsStop = false;
+    public float upspeed = 8;   //弹跳速度
+    public float jumpforce;     //弹力
+    public int Count;           //金币数量
+    public Text CoinNum;        //金币显示
 
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        rb = this.GetComponent<Rigidbody2D>();
-    }
-
-    // Update is called once per frame
     void Update()
     {
         Movement();
         SwitchAnim();
     }
 
-    void Movement()
+
+    void Movement()     //玩家操作
     {
-        //float horizontalmove = Input.GetAxis("Horizontal");     //-1向左，0不动，1向右
-        //float facedirection = Input.GetAxisRaw("Horizontal");
-
-        //if (IsStop == false)
-        //{
-        //    //一直向右运动
-        //    rb.transform.Translate(speed * Time.deltaTime, 0, 0);
-        //}
-
-        /*
-        // 角色移动
-        if (horizontalmove != 0)
-        {
-            rb.velocity = new Vector2(horizontalmove * speed, rb.velocity.y);
-            //anim.SetFloat("running", Mathf.Abs(facedirection));
-        }
-        
-
-        // 角色跳跃
-        if (Input.GetButtonDown("Jump"))
-        {
-            rb.velocity = new Vector2(rb.velocity.x, jumpforce * Time.deltaTime);
-            anim.SetBool("jumping", true);
-        }
-        */
-
-        float hight = GetComponent<BoxCollider2D>().bounds.size.y;
+        float hight = GetComponent<BoxCollider2D>().bounds.size.y;          //获取高度
 
         //按下空格键可以使方块跳跃
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            if (Physics2D.Raycast(transform.position, Vector2.down, hight, LayerMask.GetMask("ParkourGround")))
-            {
-                //给它一个向上的力
-                upwardForce();
-            }
-
-            if (Physics2D.Raycast(transform.position, Vector2.down, hight, LayerMask.GetMask("Ceiling")))
-            {
-                //给它一个向上的力
-                downwardForce();
-            }
+            //如果检测到在地面或者空气墙上则可以跳跃（避免出现连环跳）
+            upwardForce(hight);
         }
 
-        if (transform.localScale.x > 0.3)
+        //主角的缩小放大
+        if (transform.localScale.x > 0.9)
         {
-            //按下A键主角可以缩小
-            shrink();
+            shrink();               //按下A键主角可以缩小
         }
-        if (transform.localScale.x <= 1)
+        if (transform.localScale.x <= 1.5)
         {
-            //按下D键主角可以变大
-            //检测到方块上面是天花板则不能变大
-            if (!Physics2D.Raycast(transform.position, Vector2.up, 0.5f * hight, LayerMask.GetMask("AirWall")))
-            {
-                enlarge();
-            }
+            enlarge(hight);          //按下D键主角可以变大
         }
     }
 
-    void SwitchAnim()
+    void SwitchAnim()       //动画切换
     {
         anim.SetBool("running", false);
         if (anim.GetBool("jumping"))
@@ -108,74 +54,63 @@ public class ParkourPlayerController : MonoBehaviour
         }
     }
 
-    void shrink()
+
+    void upwardForce(float hight)       //向上的力
+    {
+        //在地面上
+        if (Physics2D.Raycast(transform.position, Vector2.down, hight, LayerMask.GetMask("ParkourGround")))
+        {
+            rb.AddForce(Vector3.up * upspeed, ForceMode2D.Impulse);      //给它一个向上的力
+        }
+        //在空气墙上
+        if (Physics2D.Raycast(transform.position, Vector2.down, hight, LayerMask.GetMask("AirWall")))
+        {
+            rb.AddForce(Vector3.up * upspeed, ForceMode2D.Impulse);      //给它一个向上的力
+        }
+    }
+
+    void shrink()           //主角缩小
     {
         if (Input.GetKey(KeyCode.A))
         {
-            transform.localScale = new Vector3(transform.localScale.x - 0.01f, transform.localScale.y - 0.01f, transform.localScale.z - 0.01f);
-            //speed = speed + 0.05f;
-            upspeed = upspeed + 0.05f;
-            ParkourEnvironmentController.speed = 13f;
-            ParkourBackgroundController.speed = 13f;
+            transform.localScale = new Vector3(transform.localScale.x - 0.01f, transform.localScale.y - 0.01f, transform.localScale.z - 0.01f);     //形态变小
+            upspeed = upspeed + 0.05f;                      //主角弹跳速度增大
+            ParkourEnvironmentController.speed = 13f;       //障碍物移动速度增大
+            ParkourBackgroundController.speed = 13f;        //背景移动速度增大
         }
     }
-    void enlarge()
+
+    void enlarge(float hight)          //主角变大
     {
-        if (Input.GetKey(KeyCode.D))
+        //检测到方块上面是空气墙则不能变大
+        if (Physics2D.Raycast(transform.position, Vector2.up, 0.5f * hight, LayerMask.GetMask("AirWall")))
         {
-            transform.localScale = new Vector3(transform.localScale.x + 0.01f, transform.localScale.y + 0.01f, transform.localScale.z + 0.01f);
-            //speed = speed - 0.05f;
-            upspeed = upspeed - 0.05f;
-            ParkourEnvironmentController.speed = 10f;
-            ParkourBackgroundController.speed = 10f;
+            return;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.localScale = new Vector3(transform.localScale.x + 0.01f, transform.localScale.y + 0.01f, transform.localScale.z + 0.01f);     //形态变大
+            upspeed = upspeed - 0.05f;                      //主角弹跳速度减小
+            ParkourEnvironmentController.speed = 10f;       //障碍物移动速度减小
+            ParkourBackgroundController.speed = 10f;        //背景移动速度减小
         }
     }
 
-    void upwardForce()
-    {
-        rb.AddForce(Vector3.up * upspeed, ForceMode2D.Impulse);
-    }
 
-    void downwardForce()
-    {
-        rb.AddForce(Vector3.up * upspeed, ForceMode2D.Impulse);
-    }
-
-    //如果发生边缘碰撞
-    //private void OnCollisionEnter2D(Collision2D collision)
-    //{
-    //    //碰到死亡边界
-    //    if (collision.collider.tag == "Die" && Hp > 0)
-    //    {
-    //        Hp = 0;     //血量为0
-    //    }
-    //    //跑道尽头的抉择
-    //    if (collision.collider.tag == "Choice")
-    //    {
-    //        Hp = 0;     //血量为0
-    //    }
-    //}
-
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)     //碰撞触发
     {
         //收集金币
         if (collision.tag == "Coin")
         {
-            Destroy(collision.gameObject);    //销毁自身
-            Count += 1;
-            CoinNum.text = Count.ToString();
+            Destroy(collision.gameObject);      //销毁自身
+            Count += 1;                         //金币数量+1
+            CoinNum.text = Count.ToString();    //金币显示
         }
+
         //死亡复活
         if (collision.tag == "Die")
         {
-            //Invoke("Restart", 0.5f);
-            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);     //重新加载当前场景
         }
-
     }
-
-    //void Restart()
-    //{
-    //    SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-    //}
 }
